8d8a075fe9868d280d1f6eabf9b5199d79a41aa7
ajlopez <webmaster@ajlopez.com>
2013-11-13 18:27:35 -0300

    First indent and name

--- test/lexer.js
+++ test/lexer.js
@@ -1,8 +1,29 @@
 
 var lexer = require('../lib/lexer');
 
+var TokenType = lexer.TokenType;
+
 exports['get empty string as null'] = function (test) {
     var lex = lexer.createLexer('');
     
     test.equal(lex.nextToken(), null);
 }
+
+exports['get indent and name'] = function (test) {
+    var lex = lexer.createLexer('name');
+    
+    var token = lex.nextToken();
+    
+    test.ok(token);
+    test.equal(token.type, TokenType.Indent);
+    test.equal(token.value, 0);
+    
+    token = lex.nextToken();
+    
+    test.ok(token);
+    test.equal(token.type, TokenType.Name);
+    test.equal(token.value, 'name');
+    
+    test.equal(lex.nextToken(), null);
+}
+
